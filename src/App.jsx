3a53028c7fbd76c525370430938ed11f53c45003
Gabriela <gabrielakborborema@gmail.com>
2024-08-02 13:20:05 -0300

    finished time travelling

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'
+import "./App.css";
+import Game from "./components/Game";
 
 function App() {
   return (
     <>
-    <h1>Hello World!</h1>
+      <Game />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
